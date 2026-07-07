6a1cf871349df350549145f86c81944c668dac95
antssanjeewa94 <antssanjeewa94@gmail.com>
2026-07-07 16:41:54 +0530

    add more routes

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
 
 echo "<h1>Welcome to My Mini Framework!</h1>";
+
+$request = $_SERVER['REQUEST_URI'];
+
+// සරල Router එකක් ක්‍රියාත්මක කිරීම
+switch ($request) {
+  case '/':
+  case '':
+    echo "<h1>Home Page</h1>";
+    break;
+
+  case '/about':
+    echo "<h1>About Us Page</h1>";
+    break;
+
+  case '/contact':
+    echo "<h1>Contact Page</h1>";
+    break;
+
+  default:
+    http_response_code(404);
+    echo "<h1>404 Not Found</h1>";
+    break;
+}
