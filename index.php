c4975152b60f91f4f4e052f881a2d2d7e9cd07c9
antssanjeewa94 <antssanjeewa94@gmail.com>
2026-07-07 16:44:59 +0530

    use closer to route action

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <?php
 
 echo "<h1>Welcome to My Mini Framework!</h1>";
+echo "<nav><a href='/'>Home</a> | <a href='/about'>About</a> | <a href='/notfound'>Not Found</a></nav><hr>";
 
-$request_uri = $_SERVER['REQUEST_URI'];
-$base_path = parse_url($request_uri, PHP_URL_PATH);
-$request = rtrim($base_path, '/');
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-// --- CONTROLLERS ---
-function homeController()
-{
-  return "<h1>Home Page</h1><p>Welcome to Valet powered Mini Framework!</p>";
-}
-
-function aboutController()
-{
-  return "<h1>About Us</h1><p>Running smoothly on Nginx.</p>";
-}
-
-// --- ROUTER ---
+// --- ROUTER & LOGIC ---
+// අපි වෙනම තිබුණු ෆන්ක්ෂන්ස් අයින් කරලා, කෙලින්ම Array එක ඇතුළෙම ඒවා ලිව්වා.
 $routes = [
-  '' => 'homeController',
-  '/about' => 'aboutController'
+  '/' => function () {
+    return "<h1>Home Page</h1>";
+  },
+  '/about' => function () {
+    return "<h1>About Us Page</h1>";
+  }
 ];
 
+// 404 සඳහාත් නමක් නැති ෆන්ක්ෂන් එකක් Variable එකකට ගනිමු
+$notFound = function () {
+  http_response_code(404);
+  return "<h1>404 Not Found</h1>";
+};
+
+// Route එක පරීක්ෂා කර ක්‍රියාත්මක කිරීම
 if (array_key_exists($request, $routes)) {
+  // දැන් $routes[$request] එකෙන් ලැබෙන්නේ executable function එකක් (Closure)
   $action = $routes[$request];
   echo $action();
 } else {
-  http_response_code(404);
-  echo "<h1>404 - Page Not Found (Valet)</h1>";
+  echo $notFound();
 }
